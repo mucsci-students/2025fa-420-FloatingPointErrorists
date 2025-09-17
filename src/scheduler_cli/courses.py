6c10e3313109cfb53ced43ed267c6e2a23094538
TommyN17 <166442927+Tommy-Nguyen17@users.noreply.github.com>
2025-09-17 11:24:21 -0400

    Update courses.py

--- src/scheduler_cli/courses.py
+++ src/scheduler_cli/courses.py
@@ -2,7 +2,6 @@ from .json import JsonConfig
 from scheduler import load_config_from_file, CombinedConfig, SchedulerConfig, CourseConfig
 from typing import Any
 
-class courses: 
 
 @staticmethod
 def add_course(json_config: JsonConfig, course_id: str, credits: int, room: list[str], lab: list[str], conflicts: list[str], faculty: list[str]) -> None:
@@ -15,7 +14,6 @@ def add_course(json_config: JsonConfig, course_id: str, credits: int, room: list
         faculty=faculty
     )
     json_config.config.courses.append(course_config)
-
     
 
     
