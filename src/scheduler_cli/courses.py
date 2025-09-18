d99d5c2819a5f56cbf812a598b5bfdcc933297b5
TommyN17 <166442927+Tommy-Nguyen17@users.noreply.github.com>
2025-09-17 20:33:10 -0400

    implemented delete and modify courses functions
    the functions for mod course and delete courses have been implemented

--- src/scheduler_cli/courses.py
+++ src/scheduler_cli/courses.py
@@ -14,7 +14,34 @@ def add_course(json_config: JsonConfig, course_id: str, credits: int, room: list
         faculty=faculty
     )
     json_config.config.courses.append(course_config)
+
+@staticmethod
+def mod_course(json_config: JsonConfig, course_id: str, credits: int, room: list[str], lab: list[str], conflicts: list[str], faculty: list[str]):
+    course_config = CourseConfig(
+        course_id=course_id,
+        credits=credits,
+        room=room,
+        lab=lab,
+        conflicts=conflicts,
+        faculty=faculty
+    )
+    
+    course_index: int = json_config.config.courses.index(course_config)
+    json_config.config.courses.__setitem__(course_index, course_config)
+        
     
 @staticmethod
-def del_course():
+def del_course(json_config: JsonConfig, course_id: str, credits: int, room: list[str], lab: list[str], conflicts: list[str], faculty: list[str]):
+    course_config = CourseConfig(
+        course_id=course_id,
+        credits=credits,
+        room=room,
+        lab=lab,
+        conflicts=conflicts,
+        faculty=faculty
+    )
+
+    json_config.config.courses.remove(course_config)
+
+
     
