95eb31ea9300b57fd2c68c8bed18120c2f483f49
BuildTools <unconfigured@null.spigotmc.org>
2025-09-19 00:55:25 -0400

    made functions in room class static

--- src/scheduler_cli/room.py
+++ src/scheduler_cli/room.py
@@ -4,13 +4,15 @@ from typing import Any
 Module for handling Rooms and Labs in the config files
 """
 class Room:
+    @staticmethod
     def add_room(json_config: JsonConfig, newRoom: str) -> None:
         json_config.rooms.append(newRoom)
         json_config.rooms.sort()
 
+    @staticmethod
     def mod_room(json_config: JsonConfig, room: str, newRoom: str):
         json_config.rooms[json_config.rooms.index(room)] = newRoom
 
-
+    @staticmethod
     def del_room(json_config: JsonConfig, room: str):
         json_config.rooms.remove(room)
