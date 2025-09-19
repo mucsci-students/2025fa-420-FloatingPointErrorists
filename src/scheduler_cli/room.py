389ff80fa3ea8eaa584b719aa6e7e3c84719971c
BuildTools <unconfigured@null.spigotmc.org>
2025-09-19 01:04:10 -0400

    updated room function logic to use updated typing

--- src/scheduler_cli/room.py
+++ src/scheduler_cli/room.py
@@ -6,13 +6,13 @@ Module for handling Rooms and Labs in the config files
 class Room:
     @staticmethod
     def add_room(json_config: JsonConfig, newRoom: str) -> None:
-        json_config.rooms.append(newRoom)
-        json_config.rooms.sort()
+        json_config.scheduler_config.rooms.append(newRoom)
+        json_config.scheduler_config.rooms.sort()
 
     @staticmethod
-    def mod_room(json_config: JsonConfig, room: str, newRoom: str):
-        json_config.rooms[json_config.rooms.index(room)] = newRoom
+    def mod_room(json_config: JsonConfig, room: str, newRoom: str) -> None:
+        json_config.scheduler_config.rooms[json_config.rooms.index(room)] = newRoom
 
     @staticmethod
-    def del_room(json_config: JsonConfig, room: str):
-        json_config.rooms.remove(room)
+    def del_room(json_config: JsonConfig, room: str) -> None:
+        json_config.scheduler_config.rooms.remove(room)
