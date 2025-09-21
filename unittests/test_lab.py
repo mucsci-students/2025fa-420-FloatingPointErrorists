401cc693c878b37fa27452baa403703eadf87738
BuildTools <unconfigured@null.spigotmc.org>
2025-09-20 20:34:11 -0400

    Added some unit tests for my code

---
+++ unittests/test_lab.py
@@ -0,0 +1,20 @@
+import json
+from pathlib import Path
+from click.testing import CliRunner
+from scheduler_cli import cli
+from scheduler_cli.lab import Lab
+
+CONFIG_KEY = "config"
+
+#Tests to run: Add new, Try to add duplicate, Delete existing, Delete nonexisting, mod existing, mod non existing
+
+def dummy_path() -> str:
+    return str(Path(__file__).parent / "dummy.json")
+
+def test_room_add():
+    runner = CliRunner()
+    obj = {}
+    runner.invoke(cli, ["load", dummy_path()], obj=obj)
+    jsonObj = obj[CONFIG_KEY]
+    Lab.add_lab(jsonObj, "Test Lab")
+    assert jsonObj.scheduler_config.labs.count("Test Lab") == 1
