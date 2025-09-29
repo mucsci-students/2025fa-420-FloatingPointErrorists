da0323e063fa54fcc4f590bc550a884f7a318090
Ian Oister <imoister05@gmail.com>
2025-09-29 17:43:02 -0400

    fixed foolish mistakes, just setting up project structure

--- src/scheduler_cli/gui/main.py
+++
@@ -1,9 +0,0 @@
-from base_gui import SimpleApp
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = SimpleApp()
-    window.show()
-    sys.exit(app.exec_())
