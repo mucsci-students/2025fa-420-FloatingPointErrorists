bfe0ce4a26f1c053336d974768c1e51a001bf248
TommyN17 <tommyn0017@gmail.com>
2025-10-05 23:09:49 -0400

    Separation of Concerns
    Created functioning test.py that basically adds separation of concerns. Controller shouldn't be worried about what the view and models do so the test.py file is an example to work off of. If we want to modify a label for example, we can do what's in this file and add more functions to keep modifying stuff

--- src/scheduler_cli/view/view.py
+++ src/scheduler_cli/view/view.py
@@ -1,6 +1,6 @@
-from base_gui import SimpleGUI
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+from base_gui import SimpleGUI
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
