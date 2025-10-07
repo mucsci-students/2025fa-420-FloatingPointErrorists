638ef534c3db354aeb17db3d62d0f1df31f1fc3a
Zac Tawfick <101304267+ZikoGotGame@users.noreply.github.com>
2025-10-07 00:27:55 -0400

    implemented flags.

--- src/scheduler_config_editor/main.py
+++ src/scheduler_config_editor/main.py
@@ -1,9 +1,15 @@
 import sys
-import click
 from .base_cli import cli, apply_signal_handlers
 
 def main() -> None:
-    if "-cli" in sys.argv:
+    is_cli_mode = False
+    for arg in sys.argv[1:]:
+        if arg != "-cli":
+            print(f"Error: Invalid argument: {arg}")
+            sys.exit(1)
+        else:
+            is_cli_mode = True
+    if is_cli_mode:
         sys.argv.remove("-cli")
         apply_signal_handlers()
         cli()
