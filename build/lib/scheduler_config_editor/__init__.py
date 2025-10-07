4acf9bf2020731b773a8915d7b6caa05e26fa21f
Zac Tawfick <101304267+ZikoGotGame@users.noreply.github.com>
2025-10-07 01:33:41 -0400

    Removed build directory.

--- build/lib/scheduler_config_editor/__init__.py
+++
@@ -1,8 +0,0 @@
-from .base_cli import cli
-from scheduler_config_editor.model.courses import Course
-from scheduler_config_editor.model.faculty import Faculty
-from scheduler_config_editor.model.room import Room
-from scheduler_config_editor.model.lab import Lab
-from scheduler_config_editor.model.json import JsonConfig
-
-__all__ = ["cli", "Course", "Faculty", "Room", "Lab", "JsonConfig"]
