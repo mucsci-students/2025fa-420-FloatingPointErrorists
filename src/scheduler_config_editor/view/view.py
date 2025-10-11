cd2e3cdd8917b14b2bcd01ef409032e9ecf7e946
Ian Oister <imoister05@gmail.com>
2025-10-11 17:32:48 -0400

    deleted unnecessary directory

--- src/scheduler_config_editor/view/view.py
+++ src/scheduler_config_editor/view/view.py
@@ -1,6 +1,6 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
-from scheduler_config_editor.view.base_gui import SimpleGUI, SimpleTabs
+from PyQt6.QtWidgets import QApplication
+from scheduler_config_editor.view.base_gui import SimpleGUI
 
 def view() -> None:
     app = QApplication(sys.argv)
